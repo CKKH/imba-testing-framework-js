da71e0927c71aa8a566121ccacf5248ea5ff33be
CKKH <chriskkhassan@gmail.com>
2019-01-26 11:06:49 +0000

    Testing 'toEqual' matcher built, example implementation provided

--- spec/SquareSpec.js
+++ spec/SquareSpec.js
@@ -5,3 +5,12 @@ describe('Use it blocks after descriptions to make it obvious what specific test
   it('I provide more information about the specific functionality I am testing', () => {
   })
 })
+
+describe('Square', () => {
+  it('.area calulates surface area', () => {
+    let square = new Square(5)
+    let surfaceArea = square.area()
+    console.log(surfaceArea)
+    expect(surfaceArea).toEqual(25)
+  })
+})
