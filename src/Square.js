c0b7ae8f5ddcd9eccc7269382633879f3ebed19f
CKKH <chriskkhassan@gmail.com>
2019-01-26 10:36:21 +0000

    Build basic class, testing framework to be built around this

---
+++ src/Square.js
@@ -0,0 +1,24 @@
+class Square {
+  constructor(size) {
+    if (isNaN(size)) {
+        throw new Error('argument is not a number');
+    }
+    if (size < 0) {
+        throw new Error('size should be a positive number');
+    }
+    this.size = size;
+  }
+
+  area() {
+    return this.size * this.size;
+  }
+}
+
+
+// function Square (size) {
+//   this.size = size
+// }
+
+// Square.prototype.area = function () {
+//   return this.size * this.size
+// }
