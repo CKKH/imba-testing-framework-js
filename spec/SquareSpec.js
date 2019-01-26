f2a14402bd154f32eab257d33c14303d1d023187
CKKH <chriskkhassan@gmail.com>
2019-01-26 10:48:23 +0000

    Testing 'it' block code built, example implementation provided

--- spec/SquareSpec.js
+++ spec/SquareSpec.js
@@ -1,2 +1,7 @@
 describe('Descriptions of tests added like this', () => {
 })
+
+describe('Use it blocks after descriptions to make it obvious what specific tests are doing', () => {
+  it('I provide more information about the specific functionality I am testing', () => {
+  })
+})
